57d6e653fc7d621dff55b7577e78714242317741
@metadadosjor <metadadosjor@marmota.org>
2017-05-18 10:54:51 -0300

    Version: 0.1.0
    Remoção dos comentários em posts e páginas

---
+++ single.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying all single posts and attachments
+ *
+ * @package Conecta - Cyanotype Child
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			get_template_part( 'content', 'single' );
+
+			// Previous/next post navigation.
+			the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'cyanotype' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'cyanotype' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'cyanotype' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'cyanotype' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+			) );
+
+		// End the loop.
+		endwhile;
+		?>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
