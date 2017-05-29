be1d0a98885ac098a55b7d1055d8b0702d59b611
@metadadosjor <metadadosjor@marmota.org>
2017-05-29 12:53:58 -0300

    Ajuste na imagem da home

--- content.php
+++ content.php
@@ -15,13 +15,11 @@
 				<span class="posted-on"><?php get_time_string(); ?></span>
 			</div><!-- .entry-date -->
 		<?php endif; ?>
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header>
 	<?php if ( has_post_thumbnail()) : ?>
 	<?php the_post_thumbnail('homepage-thumb', array('class' => 'alignright')); ?>
 	<?php endif; ?>
-
-
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header>
 		<?php echo the_excerpt(); ?> 
 
 </article><!-- #post-## -->
